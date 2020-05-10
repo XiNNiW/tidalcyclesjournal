d4599898c99df3a1133792955fd4620e56f82ef7
David Minnix <dlminnix09@gmail.com>
2020-05-09 22:12:45 -0400

    forgot to commit for a while

--- CustomTidalFunctions.hs
+++ CustomTidalFunctions.hs
@@ -1,74 +1,6 @@
-
-
-:set -XOverloadedStrings
-:set prompt ""
-:set prompt-cont ""
-
-
 import Sound.Tidal.Context
-
 import Sound.Tidal.Chords
-
 import Sound.Tidal.Utils
-
--- total latency = oLatency + cFrameTimespan
-tidal <- startTidal (superdirtTarget {oLatency = 0.3, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
-
-
--- total latency = oLatency + cFrameTimespan
-
--- tidal <- startTidal (superdirtTarget {oLatency = 0.02}) (defaultConfig {cFrameTimespan = 1/20, cTempoAddr = "192.168.0.105"})
-
-:{
-let p = streamReplace tidal
-    hush = streamHush tidal
-    list = streamList tidal
-    mute = streamMute tidal
-    unmute = streamUnmute tidal
-    solo = streamSolo tidal
-    unsolo = streamUnsolo tidal
-    once = streamOnce tidal
-    first = streamFirst tidal
-    asap = once
-    nudgeAll = streamNudgeAll tidal
-    all = streamAll tidal
-    resetCycles = streamResetCycles tidal
-    setcps = asap . cps
-    xfade i = transition tidal True (Sound.Tidal.Transition.xfadeIn 4) i
-    xfadeIn i t = transition tidal True (Sound.Tidal.Transition.xfadeIn t) i
-    histpan i t = transition tidal True (Sound.Tidal.Transition.histpan t) i
-    wait i t = transition tidal True (Sound.Tidal.Transition.wait t) i
-    waitT i f t = transition tidal True (Sound.Tidal.Transition.waitT f t) i
-    jump i = transition tidal True (Sound.Tidal.Transition.jump) i
-    jumpIn i t = transition tidal True (Sound.Tidal.Transition.jumpIn t) i
-    jumpIn' i t = transition tidal True (Sound.Tidal.Transition.jumpIn' t) i
-    jumpMod i t = transition tidal True (Sound.Tidal.Transition.jumpMod t) i
-    mortal i lifespan release = transition tidal True (Sound.Tidal.Transition.mortal lifespan release) i
-    interpolate i = transition tidal True (Sound.Tidal.Transition.interpolate) i
-    interpolateIn i t = transition tidal True (Sound.Tidal.Transition.interpolateIn t) i
-    clutch i = transition tidal True (Sound.Tidal.Transition.clutch) i
-    clutchIn i t = transition tidal True (Sound.Tidal.Transition.clutchIn t) i
-    anticipate i = transition tidal True (Sound.Tidal.Transition.anticipate) i
-    anticipateIn i t = transition tidal True (Sound.Tidal.Transition.anticipateIn t) i
-    forId i t = transition tidal False (Sound.Tidal.Transition.mortalOverlay t) i
-    d1 = p 1 . (|< orbit 0)
-    d2 = p 2 . (|< orbit 1)
-    d3 = p 3 . (|< orbit 2)
-    d4 = p 4 . (|< orbit 3)
-    d5 = p 5 . (|< orbit 4)
-    d6 = p 6 . (|< orbit 5)
-    d7 = p 7 . (|< orbit 6)
-    d8 = p 8 . (|< orbit 7)
-    d9 = p 9 . (|< orbit 8)
-    d10 = p 10 . (|< orbit 9)
-    d11 = p 11 . (|< orbit 10)
-    d12 = p 12 . (|< orbit 11)
-    d13 = p 13
-    d14 = p 14
-    d15 = p 15
-    d16 = p 16
-
-:}
 
 setbpm a = setcps (a/60/4)
 -- e = euclidFull
@@ -109,8 +41,8 @@ chordList = unwords $ map fst (chordTable :: [(String, [Int])])
 
 
 
--- slowrun :: (Real a, Enum a)=>Pattern a -> Pattern a
--- slowrun n = slow (fmap toTime n) $ run ( n)
+slowrun :: (Real a, Enum a)=>Pattern a -> Pattern a
+slowrun n = slow (fmap toTime n) $ run ( n)
 
 
 
@@ -143,11 +75,12 @@ terr start stop pattern = (start, stop, pattern)
 
 mode n scaleName p = (|- (scale scaleName (n-1))) $ scale scaleName $ (+ n) $ p
 
-invert p = (|* (-1)) $ p
+invert p = (|* "-1") $ p
 
 mix fx p = ( p + fx p)
 
 condFx bools fx p = sew bools (fx p) p
+
 
 foldmod ::  [Pattern Time] -> Int -> Int -> (Pattern b -> Pattern b) -> Pattern b -> Pattern b
 foldmod [] _ _ _ p = p
@@ -201,26 +134,3 @@ sendMidiClock = p "clock" $ fast 2 $ midicmd "midiClock*48" #s "midi";
 sendMidiStop = once $ midicmd "stop" #s "midi"
 
 sendMidiStart = once $ midicmd "stop" #s "midi"
-
-
-
-:{
-let setI = streamSetI tidal
-    setF = streamSetF tidal
-    setS = streamSetS tidal
-    setR = streamSetI tidal
-    setB = streamSetB tidal
-:}
-
-:set prompt "tidal> "
-
-
-
-
--- :module Sound.Tidal.Context
--- -- -- :set prompt-cont ""
--- --
--- -- import Sound.Tidal.Context
--- -- import Sound.Tidal.Chords
---
--- :load "~/Music/tidal-cycles/lib"
